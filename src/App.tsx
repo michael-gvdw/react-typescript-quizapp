80b1359ac564b6d8435a71fca5f802c610d9b554
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-04-23 11:52:52 +0200

    added ts functionality next styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+import { fetchQuizQuestions } from './Api'
+
+// components
+import QuestionCard from './components/QuestionCard'
+
+// types
+import { Difficulty, QuestionState } from './Api'
+
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+
+const TOTAL_QUESTIONS = 10
+
+
+const App = () => {
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [questions, setQuestions] = useState<QuestionState[]>([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  // console.log(fetchQuizQuestions(TOTAL_QUETSIONS, Difficulty.EASY))
+
+  const startQuiz = async () => {
+    setIsLoading(true)
+    setGameOver(false)
+
+    const newQuestinos = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+    setQuestions(newQuestinos)
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0)
+    setIsLoading(false)
+  }
+
+  const checkAnwer = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      // user answer
+      const answer = event.currentTarget.value
+      // check answer against correct answer
+      const correct = questions[number].correct_answer === answer
+      // add score if answer is correct
+      if (correct) setScore(prev => prev + 1)
+      // save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+
+      setUserAnswers(prev => [...prev, answerObject])
+    }
+  }
+
+  const nextQusetion = () => {
+    // move to next question
+    const nextQuestion = number + 1
+
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true)
+    }
+    else {
+      setNumber(nextQuestion)
+    }
+  }
+
   return (
-    <div className="App">
-      Quiz
+    <div>
+      <h1>Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className={`start`} onClick={startQuiz}>Start Quiz</button>
+      ) : null}
+
+      {!gameOver ? <p className={`score`}>Score:</p> : null }
+      
+      {isLoading && <p>Loading Questions...</p>}
+      
+      {!isLoading && !gameOver && (
+        <QuestionCard 
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnwer}
+        />
+      )}
+
+      {!gameOver && !isLoading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className={`next`} onClick={nextQusetion}>Next Question</button>
+      ) : null }
+      
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
 
+type Props = {
+    question: string;
+    answers: string[];
+    callback: any;
+    userAnswer: any;
+    questionNr: number;
+    totalQuestions: number;
+}
 
-const QuestionCard = () => {
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) => {
     return (
-        <div>Question Card</div>
+        <div>
+            <p className={`number`}>Question: {questionNr} / {totalQuestions}</p>
+
+            <p dangerouslySetInnerHTML={{ __html: question }} />
+
+            <div>
+                {answers.map(answer => (
+                    <div key={answer}>
+                        <button disabled={userAnswer} value={answer} onClick={callback}> <span dangerouslySetInnerHTML={{ __html: answer }} /> </button>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
